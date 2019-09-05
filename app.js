feb8ab411ce9a8c3167df9d2481230b355f6c6e5
Julanick <julanick@gmail.com>
2019-09-05 10:01:00 +0300

    fix: fix header jquery

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 $(function() {
- 
- let header = $("#header");
- let intro = $("#intro");
- let introH = intro.innerHeight();
- let scrillPos = $(window).scrollTop();
+  let header = $("#header");
+  let intro = $("#intro");
+  let introH = intro.innerHeight();
+  let scrillPos = $(window).scrollTop();
 
- $(window).on("scroll", function(){
-    
+  $(window).on("scroll load", function() {
+    introH = intro.innerHeight();
     scrillPos = $(this).scrollTop();
-     if( scrillPos > introH ){
-        header.addClass("fixed");
-
-     }
-     else {
-        header.removeClass("fixed");
-
-     }
-    
-});
-
- 
-
-
-
+    if (scrillPos > introH) {
+      header.addClass("fixed");
+    } else {
+      header.removeClass("fixed");
+    }
+  });
 });
