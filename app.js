9a3702a5bcd594cd02352dc0e39f1ac3bf952f6d
Julanick <julanick@gmail.com>
2019-09-05 11:15:41 +0300

    feat: add smooth scroll and nav Toggle jquery

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
 $(function() {
-  let header = $("#header");
-  let intro = $("#intro");
-  let introH = intro.innerHeight();
-  let scrillPos = $(window).scrollTop();
+    /* Fixed Header*/
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    checkScroll(scrollPos, introH);
 
-  $(window).on("scroll load", function() {
-    introH = intro.innerHeight();
-    scrillPos = $(this).scrollTop();
-    if (scrillPos > introH) {
-      header.addClass("fixed");
-    } else {
-      header.removeClass("fixed");
+    $(window).on("scroll load", function() {
+        introH = intro.innerHeight();
+        scrillPos = $(this).scrollTop();
+        checkScroll(scrollPos, introH);
+    });
+
+    function checkScroll(scrollPos, introH) {
+        if (scrollPos > introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
     }
-  });
+
+    /* Smooth scroll*/
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+        let elementId = $(this).data("scroll");
+        let elementOffSet = $(elementId).offset().top;
+        nav.removeClass("show");
+        $("html, body").animate({
+            scrollTop: elementOffSet - 70
+        }, 700);
+        console.log(elementOffSet);
+    });
+
+    /* Nav Toggle*/
+
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+    navToggle.on("click", function(event) {
+        event.preventDefault();
+        nav.toggleClass("show");
+    });
 });
